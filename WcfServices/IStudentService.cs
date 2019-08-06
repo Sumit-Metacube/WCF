219666a802923a210c2e578061df59dcc0bb69f6
todessss <44155179+todessss@users.noreply.github.com>
2019-08-06 11:28:07 +0530

    Add files via upload

---
+++ WcfServices/IStudentService.cs
@@ -0,0 +1,35 @@
+using System.ServiceModel;
+using WcfServiceLibrary.Common.Models;
+using WcfServiceLibrary.Models;
+
+namespace WcfServiceLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IStudentService
+    {
+        [OperationContract]
+        OperationResult CreateStudent(Student student);
+
+        [OperationContract]
+        [FaultContract(typeof(MyException))]
+        GetStudentsResponseModel GetStudents();
+
+        [OperationContract]
+        OperationResult UpdateStudent(Student student);
+
+        [OperationContract]
+        OperationResult DeleteStudent(DeleteStudentRequestModel student);
+    }
+
+    [DataContract]
+    public class MyException
+    {
+        private string strReason;
+        public string Reason
+        {
+            get { return strReason; }
+            set { Reason = value; }
+        }
+    }
+}
